88cc8e0025d4910810770787313a0a79b550bc5d
Julius Liias <julius.eliel.liias@gmail.com>
2023-01-09 10:05:50 +0200

    Commiting test changes

--- App.js
+++ App.js
@@ -2,9 +2,10 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  console.log('Starting example-app.')
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Test test test!</Text>
       <StatusBar style="auto" />
     </View>
   );
