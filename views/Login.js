a34e19303d6138b8d4cb128206eee735763421fb
Julius Liias <julius.eliel.liias@gmail.com>
2023-01-16 10:51:52 +0200

    Added dummytoken checking and AsyncStorage for saving log in state

--- views/Login.js
+++ views/Login.js
@@ -1,14 +1,37 @@
-import React, {useContext} from 'react';
+import React, {useContext, useEffect} from 'react';
 import {StyleSheet, View, Text, Button} from 'react-native';
 import PropTypes from 'prop-types';
 import {MainContext} from '../contexts/MainContext';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const Login = ({navigation}) => {
   const {setIsLoggedIn} = useContext(MainContext);
-  const logIn = () => {
+  const logIn = async () => {
     console.log('Logging in!');
     setIsLoggedIn(true);
+    try {
+      await AsyncStorage.setItem('userToken', 'abc123');
+    } catch (error) {
+      console.warn('Error in storing the user token: ', error);
+    }
   };
+
+  const checkToken = async () => {
+    try {
+      const userToken = await AsyncStorage.getItem('userToken');
+
+      if (userToken === 'abc123') {
+        setIsLoggedIn(true);
+      }
+    } catch (error) {
+      console.log('No valid token available', error);
+    }
+  };
+
+  useEffect(() => {
+    checkToken();
+  }, []);
+
   return (
     <View style={styles.container}>
       <Text>Login</Text>
